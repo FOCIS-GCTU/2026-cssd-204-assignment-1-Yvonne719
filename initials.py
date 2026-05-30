bf5377089677a6ba46189f6cf0e9b58b6a6a8af7
Princess Yvonne Eshun <princessyveshun@gmail.com>
2026-05-30 22:18:15 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+#File: initials.py
+#Description: A program that prints large letters of my initials (PYE)
+#Assignment Number: 1
+#
+#Name: Princess Yvonne Eshun
+#STUDENT ID: 2425403955
+#Email: 2425403955@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor, Princess Yvonne Eshun, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    """
+    Prints out a rectangular grid containing small and large letters of my initials
+    """
+    print()
+    print("...PYE")
+    print()
+    print("...PPPPPPPPPP..........YYY......YYY........EEEEEEEEEEEE.....")
+    print("...PPPPPPPPPPP..........YYY....YYY.........EEE..............")
+    print("...PPP......PPP..........YYY..YYY..........EEE..............")
+    print("...PPP......PPP...........YY..YY...........EEE..............")
+    print("...PPPPPPPPPPP.............YYYY............EEEEEEEEEE.......")
+    print("...PPPPPPPPPP..............YYYY............EEE..............")
+    print("...PPP.....................YYYY............EEE..............")
+    print("...PPP.....................YYYY............EEE..............")
+    print("...PPP............**.......YYYY.......**...EEE............**")
+    print("...PPP............**.......YYYY.......**...EEEEEEEEEEEE...**")
+    print()
 
 
 main()
